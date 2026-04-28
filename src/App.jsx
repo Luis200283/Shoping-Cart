f167d97dcf1db1f611d7911eb69f60cc7f8d14da
Luis Hernandez <luishernandezopsu2019@gmail.com>
2026-04-28 19:50:50 -0400

    initial feat setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline ">Hello, Vite + React!</h1>
+      <div className="text-green-600 text-5xl ">Hola</div>
+    </>
+  );
+}
+
+export default App;
